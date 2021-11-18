03a12378974ffac85cfd7c3eb85a63f75b2cbcf2
sanntintaung <sanntintaung009@gmail.com>
2021-11-18 10:36:35 +0630

    Blog & Footer

--- src/Components/article/Article.jsx
+++ src/Components/article/Article.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './article.css'
 
-const Article = () => {
+const Article = ({img,date,title}) => {
     return (
-        <div>
-
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-img">
+                <img src={img} alt=""/>
+            </div>
+            <div className="gpt3__blog-container_article-content">
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read more</p>
+            </div>
         </div>
     );
 };
--- src/Containers/blog/Blog.jsx
+++ src/Containers/blog/Blog.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import Article from "../../Components/article/Article";
+import { blog1,blog2,blog3,blog4,blog5 } from './imports';
+import './blog.css'
 
+const data = {
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us explore how it is?'
+}
 const Blog = () => {
     return (
-        <div>
-
+        <div className="gpt3__blog section__padding" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">A lot is happening,<br/>We are blogging about it.</h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className="gpt3__blog-groupA">
+                    <Article img={blog1} date={data.date} title={data.title}/>
+                </div>
+                <div className="gpt3__blog-groupB">
+                    <Article img={blog2} date={data.date} title={data.title}/>
+                    <Article img={blog3} date={data.date} title={data.title}/>
+                    <Article img={blog4} date={data.date} title={data.title}/>
+                    <Article img={blog5} date={data.date} title={data.title}/>
+                </div>
+            </div>
         </div>
     );
 };
